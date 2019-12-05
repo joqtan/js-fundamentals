011d57f60b4e724ffd5b8329a4f306f1a8206137
joqtan <joqtanmarmol@gmail.com>
2019-12-05 02:29:04 -0600

    js-3

---
+++ lesson14/index.js
@@ -0,0 +1,18 @@
+function Persona(nombre, apellido, altura){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+}
+Persona.prototype.saludar = function(){
+    console.log(`hola me llamo ${this.nombre} ${this.apellido}`)
+}
+
+var joqtan = new Persona('joqtan','marmol',1.65)
+var javier = new Persona('javier','mejia',1.69)
+
+Persona.prototype.soyAlto = () => {
+    debugger
+    return this.altura > 1.6
+}
+joqtan.soyAlto()
+javier.soyAlto()
---
+++ lesson15/index.js
@@ -0,0 +1,23 @@
+function Persona(nombre, apellido, altura){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+}
+Persona.prototype.saludar = function(){
+    console.log(`hola me llamo ${this.nombre} ${this.apellido}`)
+}
+
+var joqtan = new Persona('joqtan','marmol',1.65)
+var javier = new Persona('javier','mejia',1.69)
+
+Persona.prototype.soyAlto = function(){
+    return this.altura > 1.6
+}
+
+function Desarrollador(nombre,apellido){
+    this.nombre = nombre
+    this.apellido = apellido
+}
+
+joqtan.soyAlto()
+javier.soyAlto()
